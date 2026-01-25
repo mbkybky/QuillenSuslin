18e85e6a619efc8877b2dadd1445cc54a700e700
Yongle Hu <140475041+mbkybky@users.noreply.github.com>
2026-01-25 10:45:17 +0800

    Statement of lemma exact_seq

--- QuillenSuslin/StablyFree.lean
+++ QuillenSuslin/StablyFree.lean
@@ -1,10 +1,26 @@
 import Mathlib
 
+universe u v
+
 /-- A module $P$ over a ring $R$ is \textit{stably free} if there exists a free finitely generated
   module $F$ over $R$ such that $P \oplus F$ is a free module. -/
-def IsStablyFree (R : Type) (P : Type) [CommRing R] [AddCommGroup P] [Module R P] : Prop :=
-    ∃ (N : Type) (_ : AddCommGroup N) (_ : Module R N),
+def IsStablyFree (R P : Type*) [CommRing R] [AddCommGroup P] [Module R P] : Prop :=
+    ∃ (N : Type v) (_ : AddCommGroup N) (_ : Module R N),
     Module.Finite R N ∧ Module.Free R N ∧ Module.Free R (P × N)
+
+/-- Let $R$ be a ring. Let $0 \to P' \to P \to P'' \to 0$ be a short
+	exact sequence of finite projective $R$-modules. If $2$ out of $3$
+	of these modules are stably free, then so is the third. -/
+theorem isStablyFree_two_of_three_of_shortExact (R P₁ P₂ P₃ : Type*) [CommRing R]
+    [AddCommGroup P₁] [Module R P₁] [Module.Finite R P₁] [Module.Projective R P₁]
+    [AddCommGroup P₂] [Module R P₂] [Module.Finite R P₂] [Module.Projective R P₂]
+    [AddCommGroup P₃] [Module R P₃] [Module.Finite R P₃] [Module.Projective R P₃]
+    {f : P₁ →ₗ[R] P₂} {g : P₂ →ₗ[R] P₃} (hf : Function.Injective f) (hg : Function.Surjective g)
+    (h : Function.Exact f g) :
+      (IsStablyFree R P₁ → IsStablyFree R P₂ → IsStablyFree R P₃) ∧
+        (IsStablyFree R P₁ → IsStablyFree R P₃ → IsStablyFree R P₂) ∧
+          (IsStablyFree R P₂ → IsStablyFree R P₃ → IsStablyFree R P₁) := by
+  sorry
 
 /-
 \begin{definition}
