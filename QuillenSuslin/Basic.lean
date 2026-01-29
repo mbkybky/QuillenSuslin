f1a555bfa385401bd26dd0c577f3bd90c4356374
Yongle Hu <140475041+mbkybky@users.noreply.github.com>
2026-01-29 22:03:50 +0800

    Update Basic.lean

--- QuillenSuslin/Basic.lean
+++ QuillenSuslin/Basic.lean
@@ -1,2 +1,25 @@
 import QuillenSuslin.UnimodularVector
 import QuillenSuslin.StablyFree
+
+variable (R : Type*) [CommRing R]
+
+theorem module_free_of_isStablyFree_of_unimodularVectorEquiv
+    (hR : ∀ {s : Type} [Fintype s] [DecidableEq s] (o : s) {v : s → R} (_ : IsUnimodular v),
+      UnimodularVectorEquiv v (fun i => if i = o then 1 else 0))
+    (P : Type*) [AddCommGroup P] [Module R P] (h : IsStablyFree R P) :
+    Module.Free R P := by
+  sorry
+
+/-
+\begin{theorem}[Quillen-Suslin]\label{thm:13}
+	A finitely generated projective module over $k[x_1, \dots, x_n]$ for $k$ a principal ideal domain is free.
+\end{theorem}
+
+\begin{proof}
+	By Corollary \ref{cor:3}, we only need to show that a stably free module over $R = k[x_1, \dots, x_n]$ is free. That is, if $P$ is such a finitely generated module such that $P \oplus R^m \simeq R^{m'}$, then $P$ is free. By induction on $m$, one reduces to the case $m = 1$. In this case we have an exact sequence
+	$$ \displaystyle 0 \rightarrow R \rightarrow R^{m'} \rightarrow P \rightarrow 0 $$
+	and we have to conclude that the cokernel $P$ is free.
+
+	But the injection $R \rightarrow R^{m'}$ corresponds to a unimodular vector, and we have seen [by Theorem \ref{thm:12}] that this is isomorphic to the standard embedding $e_1: R \rightarrow R^{m'}$, whose cokernel is obviously free. Thus $P$ is free.
+\end{proof}
+-/
