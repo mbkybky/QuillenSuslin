f272c2f9689a284a28a6015c09fa08f58b7c3768
Yongle Hu <140475041+mbkybky@users.noreply.github.com>
2026-03-15 13:51:52 +0800

    Update Localization.lean

--- QuillenSuslin/FiniteFreeResolution/Localization.lean
+++ QuillenSuslin/FiniteFreeResolution/Localization.lean
@@ -1,29 +1,24 @@
-import Mathlib.RingTheory.RegularLocalRing.GlobalDimension
-import Mathlib.RingTheory.LocalProperties.ProjectiveDimension
-import Mathlib.RingTheory.LocalRing.Module
-import Mathlib.RingTheory.Localization.Submodule
+import Mathlib.Algebra.Category.ModuleCat.Projective
+import Mathlib.Algebra.Homology.ShortComplex.ModuleCat
 import Mathlib.CategoryTheory.Abelian.Projective.Dimension
+import Mathlib.RingTheory.LocalProperties.Projective
 import QuillenSuslin.FiniteFreeResolution.Basic
 
 universe u
 
-open CategoryTheory
+variable (R : Type u) [CommRing R] (M : Type u) [AddCommGroup M] [Module R M]
 
-variable (R : Type u) [CommRing R]
-
+open CategoryTheory in
 theorem hasFiniteFreeResolution_of_hasProjectiveDimensionLE
-    [IsRegularLocalRing R]
-    [Small R] [IsLocalRing R] [IsNoetherianRing R]
-    {M : Type u} [AddCommGroup M] [Module R M] [Module.Finite R M] {n : ℕ}
-    [HasProjectiveDimensionLE (ModuleCat.of R M) n] :
+    [IsLocalRing R] [IsNoetherianRing R] [Module.Finite R M]
+    (n : ℕ) [HasProjectiveDimensionLE (ModuleCat.of R M) n] :
     HasFiniteFreeResolution R M := by
   induction n generalizing M with
   | zero =>
       have hproj : Projective (ModuleCat.of R M) :=
         (projective_iff_hasProjectiveDimensionLT_one _).2 inferInstance
       have hmodproj : Module.Projective R M := (IsProjective.iff_projective (R := R) M).2 hproj
-      letI : Module.Projective R M := hmodproj
-      letI : Module.Free R M := Module.free_of_flat_of_isLocalRing
+      let : Module.Free R M := Module.free_of_flat_of_isLocalRing
       exact hasFiniteFreeResolution_of_finite_of_free M
   | succ n ih =>
       obtain ⟨P, _, _, _, _, f, hf⟩ := Module.exists_finite_presentation R M
@@ -35,40 +30,34 @@ theorem hasFiniteFreeResolution_of_hasProjectiveDimensionLE
       have hker : HasProjectiveDimensionLE (ModuleCat.of R (LinearMap.ker f)) n := by
         simpa [S, HasProjectiveDimensionLE] using
           (hS.hasProjectiveDimensionLT_X₃_iff n hproj).mp inferInstance
-      letI := hker
-      letI : Module.Finite R (LinearMap.ker f) :=
+      let : Module.Finite R (LinearMap.ker f) :=
         Module.Finite.of_injective (LinearMap.ker f).subtype (LinearMap.ker f).injective_subtype
       have hfreeP : HasFiniteFreeResolution R P := hasFiniteFreeResolution_of_finite_of_free P
-      have hker' : HasFiniteFreeResolution R (LinearMap.ker f) := ih
+      have hker' : HasFiniteFreeResolution R (LinearMap.ker f) := ih _
       exact hasFiniteFreeResolution_of_shortExact_of_left_of_middle
         (P₁ := LinearMap.ker f) (P₂ := P) (P₃ := M)
-        (f := (LinearMap.ker f).subtype) (g := f)
-        (LinearMap.ker f).injective_subtype
-        hf
-        (LinearMap.exact_subtype_ker_map f)
-        hker'
-        hfreeP
+          (f := (LinearMap.ker f).subtype) (g := f)
+            (LinearMap.ker f).injective_subtype hf (LinearMap.exact_subtype_ker_map f) hker' hfreeP
+
+variable {R M}
 
 theorem hasFiniteFreeResolutionLength_localized
-    [Nontrivial R] {M : Type u} [AddCommGroup M] [Module R M] (S : Submonoid R) {n : ℕ}
-    (h : HasFiniteFreeResolutionLength R M n) :
+    [Nontrivial R] (S : Submonoid R) {n : ℕ} (h : HasFiniteFreeResolutionLength R M n) :
     HasFiniteFreeResolutionLength (Localization S) (LocalizedModule S M) n := by
   induction h with
   | zero P =>
       let b := Module.Free.chooseBasis R P
-      haveI : Finite (Module.Free.ChooseBasisIndex R P) := Module.Finite.finite_basis b
       let bS := b.ofIsLocalizedModule (Localization S) S (LocalizedModule.mkLinearMap S P)
-      letI : Module.Free (Localization S) (LocalizedModule S P) :=
+      let : Module.Free (Localization S) (LocalizedModule S P) :=
         Module.free_of_isLocalizedModule S (LocalizedModule.mkLinearMap S P)
-      letI : Module.Finite (Localization S) (LocalizedModule S P) := Module.Finite.of_basis bS
+      let : Module.Finite (Localization S) (LocalizedModule S P) := Module.Finite.of_basis bS
       exact HasFiniteFreeResolutionLength.zero (LocalizedModule S P)
   | succ P n F f hf hker ih =>
       let b := Module.Free.chooseBasis R F
-      haveI : Finite (Module.Free.ChooseBasisIndex R F) := Module.Finite.finite_basis b
       let bS := b.ofIsLocalizedModule (Localization S) S (LocalizedModule.mkLinearMap S F)
-      letI : Module.Free (Localization S) (LocalizedModule S F) :=
+      let : Module.Free (Localization S) (LocalizedModule S F) :=
         Module.free_of_isLocalizedModule S (LocalizedModule.mkLinearMap S F)
-      letI : Module.Finite (Localization S) (LocalizedModule S F) := Module.Finite.of_basis bS
+      let : Module.Finite (Localization S) (LocalizedModule S F) := Module.Finite.of_basis bS
       have hker' :
           HasFiniteFreeResolutionLength (Localization S)
             (LinearMap.ker (LocalizedModule.map S f)) n := by
@@ -86,19 +75,17 @@ theorem hasFiniteFreeResolutionLength_localized
         (LocalizedModule.map_surjective S f hf) hker'
 
 theorem hasFiniteFreeResolution_localized
-    [Nontrivial R] {M : Type u} [AddCommGroup M] [Module R M] (S : Submonoid R)
-    (h : HasFiniteFreeResolution R M) :
+    [Nontrivial R] (S : Submonoid R) (h : HasFiniteFreeResolution R M) :
     HasFiniteFreeResolution (Localization S) (LocalizedModule S M) := by
   rcases h with ⟨F, _, _, _, _, f, hf, n, hn⟩
   let b := Module.Free.chooseBasis R F
-  haveI : Finite (Module.Free.ChooseBasisIndex R F) := Module.Finite.finite_basis b
   let bS := b.ofIsLocalizedModule (Localization S) S (LocalizedModule.mkLinearMap S F)
-  letI : Module.Free (Localization S) (LocalizedModule S F) :=
+  let : Module.Free (Localization S) (LocalizedModule S F) :=
     Module.free_of_isLocalizedModule S (LocalizedModule.mkLinearMap S F)
-  letI : Module.Finite (Localization S) (LocalizedModule S F) := Module.Finite.of_basis bS
+  let : Module.Finite (Localization S) (LocalizedModule S F) := Module.Finite.of_basis bS
   have hk0 :
       HasFiniteFreeResolutionLength (Localization S) (LocalizedModule S (LinearMap.ker f)) n :=
-    hasFiniteFreeResolutionLength_localized (R := R) S hn
+    hasFiniteFreeResolutionLength_localized S hn
   let eKer :
       LocalizedModule S (LinearMap.ker f) ≃ₗ[Localization S]
         LinearMap.ker (LocalizedModule.map S f) :=
