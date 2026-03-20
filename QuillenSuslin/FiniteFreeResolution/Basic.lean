360529d3ee78aed9926b2c4da6a39660a0fedd33
Yongle Hu <140475041+mbkybky@users.noreply.github.com>
2026-03-20 21:34:55 +0800

    add small

--- QuillenSuslin/FiniteFreeResolution/Basic.lean
+++ QuillenSuslin/FiniteFreeResolution/Basic.lean
@@ -6,18 +6,18 @@ Authors: Yongle Hu
 import Mathlib.RingTheory.Finiteness.Prod
 import Mathlib.RingTheory.PicardGroup
 
-universe u v w
+universe u v α β γ
 
 /-- `HasFiniteFreeResolutionOfLength R P n` means `P` admits a free resolution of length `n`
 by finitely generated free modules. We use the convention that length `0` means `P` itself is
 finitely generated and free, and the successor step is given by a surjection from a finitely
 generated free module with kernel admitting a shorter resolution. -/
-inductive HasFiniteFreeResolutionOfLength (R : Type u) [CommRing R] :
+inductive HasFiniteFreeResolutionOfLength (R : Type u) [CommRing R] [Small.{v} R] :
     ∀ (P : Type v), [AddCommGroup P] → [Module R P] → ℕ → Prop
   | zero (P : Type v) [AddCommGroup P] [Module R P] [Module.Finite R P] [Module.Free R P] :
       HasFiniteFreeResolutionOfLength R P 0
   | succ (P : Type v) [AddCommGroup P] [Module R P] (n : ℕ)
-      (F : Type u) [AddCommGroup F] [Module R F] [Module.Finite R F] [Module.Free R F]
+      (F : Type v) [AddCommGroup F] [Module R F] [Module.Finite R F] [Module.Free R F]
       (K : Type v) [AddCommGroup K] [Module R K] [Module.Finite R K] [Module.Free R K]
       {f : K →ₗ[R] F} (hf : Function.Injective f) {g : F →ₗ[R] P} (hg : Function.Surjective g)
       (he : Function.Exact f g) (hk : HasFiniteFreeResolutionOfLength R K n) :
@@ -25,40 +25,46 @@ inductive HasFiniteFreeResolutionOfLength (R : Type u) [CommRing R] :
 
 /-- A module `P` over a commutative ring `R` has a finite free resolution if it has a resolution
 of some finite length by finitely generated free `R`-modules. -/
-def HasFiniteFreeResolution (R P : Type*) [CommRing R] [AddCommGroup P] [Module R P] : Prop :=
+def HasFiniteFreeResolution (R : Type u) [CommRing R] [Small.{v} R]
+    (P : Type v) [AddCommGroup P] [Module R P] : Prop :=
   ∃ (n : ℕ),  HasFiniteFreeResolutionOfLength R P n
 
-variable {R : Type*} [CommRing R]
+variable {R : Type u} [CommRing R] [Small.{v} R] [Small.{α} R] [Small.{β} R] [Small.{γ} R]
 
-theorem hasFiniteFreeResolutionOfLength_of_linearEquiv {P : Type v} {Q : Type w}
+theorem hasFiniteFreeResolutionOfLength_of_linearEquiv {P : Type α} {Q : Type β}
     [AddCommGroup P] [Module R P] [AddCommGroup Q] [Module R Q] (e : P ≃ₗ[R] Q)
     {n : ℕ} (hn : HasFiniteFreeResolutionOfLength R P n) : HasFiniteFreeResolutionOfLength R Q n :=
+  sorry
+
+theorem hasFiniteFreeResolution_of_linearEquiv {P : Type α} {Q : Type β}
+    [AddCommGroup P] [Module R P] [AddCommGroup Q] [Module R Q] (e : P ≃ₗ[R] Q)
+    (hn : HasFiniteFreeResolution R P) : HasFiniteFreeResolution R Q :=
   sorry
 
 section exact_seq
 
 /-- In a short exact sequence `0 → P₁ → P₂ → P₃ → 0`, if `P₁` and `P₃` have finite free
 resolutions, then so does `P₂`. -/
-theorem hasFiniteFreeResolution_of_shortExact_of_left_of_right (P₁ P₂ P₃ : Type*)
-    [AddCommGroup P₁] [Module R P₁] [AddCommGroup P₂] [Module R P₂] [AddCommGroup P₃] [Module R P₃]
-    {f : P₁ →ₗ[R] P₂} {g : P₂ →ₗ[R] P₃} (hf : Function.Injective f) (hg : Function.Surjective g)
-    (h : Function.Exact f g) (h₁ : HasFiniteFreeResolution R P₁)
+theorem hasFiniteFreeResolution_of_shortExact_of_left_of_right (P₁ : Type α) (P₂ : Type β)
+    (P₃ : Type γ) [AddCommGroup P₁] [Module R P₁] [AddCommGroup P₂] [Module R P₂] [AddCommGroup P₃]
+    [Module R P₃] {f : P₁ →ₗ[R] P₂} {g : P₂ →ₗ[R] P₃} (hf : Function.Injective f)
+    (hg : Function.Surjective g) (h : Function.Exact f g) (h₁ : HasFiniteFreeResolution R P₁)
     (h₃ : HasFiniteFreeResolution R P₃) : HasFiniteFreeResolution R P₂ := sorry
 
 /-- In a short exact sequence `0 → P₁ → P₂ → P₃ → 0`, if `P₁` and `P₂` have finite free
 resolutions, then so does `P₃`. -/
-theorem hasFiniteFreeResolution_of_shortExact_of_left_of_middle (P₁ P₂ P₃ : Type*)
-    [AddCommGroup P₁] [Module R P₁] [AddCommGroup P₂] [Module R P₂] [AddCommGroup P₃] [Module R P₃]
-    {f : P₁ →ₗ[R] P₂} {g : P₂ →ₗ[R] P₃} (hf : Function.Injective f) (hg : Function.Surjective g)
-    (h : Function.Exact f g) (h₁ : HasFiniteFreeResolution R P₁)
+theorem hasFiniteFreeResolution_of_shortExact_of_left_of_middle (P₁ : Type α) (P₂ : Type β)
+    (P₃ : Type γ) [AddCommGroup P₁] [Module R P₁] [AddCommGroup P₂] [Module R P₂] [AddCommGroup P₃]
+    [Module R P₃] {f : P₁ →ₗ[R] P₂} {g : P₂ →ₗ[R] P₃} (hf : Function.Injective f)
+    (hg : Function.Surjective g) (h : Function.Exact f g) (h₁ : HasFiniteFreeResolution R P₁)
     (h₂ : HasFiniteFreeResolution R P₂) : HasFiniteFreeResolution R P₃ := sorry
 
 /-- In a short exact sequence `0 → P₁ → P₂ → P₃ → 0`, if `P₂` and `P₃` have finite free
 resolutions, then so does `P₁`. -/
-theorem hasFiniteFreeResolution_of_shortExact_of_middle_of_right (P₁ P₂ P₃ : Type*)
-    [AddCommGroup P₁] [Module R P₁] [AddCommGroup P₂] [Module R P₂] [AddCommGroup P₃] [Module R P₃]
-    {f : P₁ →ₗ[R] P₂} {g : P₂ →ₗ[R] P₃} (hf : Function.Injective f) (hg : Function.Surjective g)
-    (h : Function.Exact f g) (h₂ : HasFiniteFreeResolution R P₂)
+theorem hasFiniteFreeResolution_of_shortExact_of_middle_of_right (P₁ : Type α) (P₂ : Type β)
+    (P₃ : Type γ) [AddCommGroup P₁] [Module R P₁] [AddCommGroup P₂] [Module R P₂] [AddCommGroup P₃]
+    [Module R P₃] {f : P₁ →ₗ[R] P₂} {g : P₂ →ₗ[R] P₃} (hf : Function.Injective f)
+    (hg : Function.Surjective g) (h : Function.Exact f g) (h₂ : HasFiniteFreeResolution R P₂)
     (h₃ : HasFiniteFreeResolution R P₃) : HasFiniteFreeResolution R P₁ := sorry
 
 end exact_seq
