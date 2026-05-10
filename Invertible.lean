332b67c549ab8ddc59664f6afbd0bb6c6b652122
Yongle Hu <140475041+mbkybky@users.noreply.github.com>
2026-05-10 10:57:56 +0800

    Update Invertible.lean

--- Invertible.lean
+++ Invertible.lean
@@ -11,7 +11,9 @@ public section
 
 variable {R : Type*} [CommRing R] {M : Type*} [AddCommGroup M] [Module R M]
 
-open TensorProduct LocalizedModule IsLocalizedModule IsLocalization
+open IsLocalizedModule IsLocalization
+
+open scoped TensorProduct
 
 theorem Module.Invertible.of_isLocalized_maximal [Module.FinitePresentation R M]
     (Rₚ : ∀ (m : Ideal R) [m.IsMaximal], Type*)
@@ -28,33 +30,27 @@ theorem Module.Invertible.of_isLocalized_maximal [Module.FinitePresentation R M]
     (h : ∀ (m : Ideal R) [m.IsMaximal], Module.Invertible (Rₚ m) (Mₚ m)) :
     Module.Invertible R M where
   bijective := by
-    sorry
+    let ϕ (m : Ideal R) [m.IsMaximal] :
+        Module.Dual R M ⊗[R] M →ₗ[R] Module.Dual (Rₚ m) (Mₚ m) ⊗[R] Mₚ m := TensorProduct.map
+      (mapExtendScalars m.primeCompl (f m) (Algebra.linearMap R (Rₚ m)) (Rₚ m)) (f m)
+    refine bijective_of_isLocalized_maximal _ ϕ Rₚ (fun m _ ↦ Algebra.linearMap R (Rₚ m))
+      (contractLeft R M) (fun m _ ↦ ?_)
+    let ψ : Module.Dual (Rₚ m) (Mₚ m) ⊗[Rₚ m] Mₚ m ≃ₗ[R] Module.Dual (Rₚ m) (Mₚ m) ⊗[R] Mₚ m :=
+      (moduleTensorEquiv m.primeCompl (Rₚ m) (Module.Dual (Rₚ m) (Mₚ m)) (Mₚ m)).restrictScalars R
+    have hψ : (map m.primeCompl (ϕ m) (Algebra.linearMap R (Rₚ m))) (contractLeft R M) =
+        (contractLeft (Rₚ m) (Mₚ m)).restrictScalars R ∘ₗ ψ.symm.toLinearMap := by
+      apply IsLocalizedModule.ext m.primeCompl (ϕ m) (map_units (Algebra.linearMap R (Rₚ m)))
+      ext α x
+      simp only [TensorProduct.AlgebraTensorModule.curry_apply, LinearMap.restrictScalars_self,
+        TensorProduct.curry_apply, LinearMap.coe_comp, Function.comp_apply,
+        LinearMap.coe_restrictScalars, LinearEquiv.coe_coe]
+      change _ = (mapExtendScalars m.primeCompl (f m) (Algebra.linearMap R (Rₚ m)) (Rₚ m)) α (f m x)
+      simp [IsLocalizedModule.mapExtendScalars]
+    simp [hψ, (h m).bijective.comp ψ.symm.bijective]
 
 theorem Module.Invertible.of_localized_maximal [Module.FinitePresentation R M]
     (h : ∀ (m : Ideal R) [m.IsMaximal],
       Module.Invertible (Localization.AtPrime m) (LocalizedModule.AtPrime m M)) :
-    Module.Invertible R M where
-  bijective := by
-    let ϕ (m : Ideal R) [m.IsMaximal] :=
-      TensorProduct.map (mkLinearMap m.primeCompl (Module.Dual R M)) (mkLinearMap m.primeCompl M)
-    refine bijective_of_isLocalized_maximal _ ϕ
-      (fun m ↦ Localization.AtPrime m) (fun _ _ ↦ mkLinearMap _ _) (contractLeft R M) (fun m _ ↦ ?_)
-    let Rₘ := Localization.AtPrime m
-    let Mₘ := LocalizedModule.AtPrime m M
-    let Dₘ := Module.Dual Rₘ (LocalizedModule.AtPrime m M)
-    simp
-    let ψ : LocalizedModule m.primeCompl (Dual R M) ⊗[R] Mₘ ≃ₗ[R] Dₘ ⊗[Rₘ] Mₘ :=
-      (Module.FinitePresentation.linearEquivMapExtendScalars m.primeCompl).rTensor Mₘ ≪≫ₗ
-        (moduleTensorEquiv m.primeCompl Rₘ Dₘ Mₘ).symm.restrictScalars R
-    have h : (map m.primeCompl (ϕ m) (mkLinearMap m.primeCompl R)) (contractLeft R M)
-        = (contractLeft Rₘ Mₘ).restrictScalars R ∘ₗ ψ.toLinearMap := by
-      apply TensorProduct.ext
-      ext f x
-      simp
-      rw [show ψ (f ⊗ₜ[R] x) =
-        (Module.FinitePresentation.linearEquivMapExtendScalars m.primeCompl f) ⊗ₜ[Rₘ] x from rfl]
-      induction x using induction_on with | _ x s
-      induction f using induction_on with | _ f t
-      simp [ϕ]
-      sorry
-    sorry
+    Module.Invertible R M :=
+  of_isLocalized_maximal (fun m _ ↦ Localization.AtPrime m) (fun m _ ↦ LocalizedModule.AtPrime m M)
+    (fun m _ ↦ LocalizedModule.mkLinearMap m.primeCompl M) h
